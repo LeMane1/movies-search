2e7384759cacba5dec24d16403aa26e154734748
Зеленин Александр <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-02-03 20:20:01 +0300

    updated ActorsBlock, added switcher buttons, added hover for PersonCard

--- src/entities/person-card/ui/PersonCard.tsx
+++ src/entities/person-card/ui/PersonCard.tsx
@@ -22,18 +22,37 @@ export const PersonCard: React.FC<IPersonCardProps> = ({
         className={className}
         css={css`
           width: 120px;
+          :hover{
+            cursor: pointer;
+          }
+          &:hover #person-image{
+            transform: scale(1.08);
+            opacity: 1;
+            transition: all .2s ease-in-out;
+            transform-origin: center;
+            }
         `}
       >
-        <img
-          loading="lazy"
-          src={person?.photo}
-          css={css`
-            width: 100px;
-            height: 150px;
-            border-radius: 10px;
-            margin-bottom: 8px;
-            object-fit: cover;
-        `} />
+        <div css={css`
+          overflow: hidden;
+          width: 100px;
+          height: 150px;
+          border-radius: 10px;
+          margin-bottom: 8px;
+        `}>
+          <img
+            loading="lazy"
+            id="person-image"
+            src={person?.photo}
+            css={css`
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+              opacity: .8;
+              transition: all .2s ease-in-out;
+          `} />
+        </div>
+
         <Title css={css`
           font-size: 14px !important;
           margin-bottom: 4px !important;
@@ -42,6 +61,7 @@ export const PersonCard: React.FC<IPersonCardProps> = ({
         `}>
           {person?.name}
         </Title>
+
         <Title css={css`
           font-size: 12px !important;
           opacity: 60%;
--- src/widgets/actors-block/ui/ActorsBlock.tsx
+++ src/widgets/actors-block/ui/ActorsBlock.tsx
@@ -1,36 +1,100 @@
-import { Typography, Flex } from "antd";
+import { useRef } from "react";
+import { Typography, Flex, Button, Grid } from "antd";
 import type { Person } from "src/shared/models"
 import { PersonCard } from "src/entities/person-card";
 import { css } from "@emotion/react";
+import {LeftOutlined, RightOutlined} from '@ant-design/icons'
 
 const { Title } = Typography
+const { useBreakpoint } = Grid
 
 interface IActorsBlockProps {
   actors?: Person[] | null;
 }
 
 export const ActorsBlock: React.FC<IActorsBlockProps> = ({ actors }) => {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const { xs } = useBreakpoint()
+
+  const scroll = (direction: "left" | "right") => {
+    const cardWidth = 128
+    let visibleCards = 0
+    const actorsBlock = document.querySelector('#actors-block-container')
+
+    if (actorsBlock && actorsBlock.clientWidth){
+      visibleCards = Math.ceil(actorsBlock.clientWidth / cardWidth)
+    }
+
+    if (scrollRef.current) {
+      const scrollAmount = cardWidth * visibleCards
+
+      scrollRef.current.scrollBy({
+        left: direction === "right" ? scrollAmount : -scrollAmount,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <>
+    <div
+      id="actors-block-container"
+      css={css`
+        background-color: #222222;
+        padding-top: 16px;
+        padding-bottom: 16px;
+        border-radius: 8px;
+        position: relative;
+    `}>
       <Title level={3}
         css={css`
           margin-bottom: 12px !important;
+          padding-left: 16px;
+          padding-right: 16px;
         `}>
         В ролях
       </Title>
 
+      <Button
+        shape="circle"
+        icon={<LeftOutlined />}
+        size={xs ? 'middle' : 'large'}
+        css={css`
+          position: absolute;
+          left: ${xs ? '-10px' : '-20px'};
+          top: 45%;
+          z-index: 1000;
+        `}
+        onClick={() => scroll("left")}
+      />
+
+      <Button
+        shape="circle"
+        size={xs ? 'middle' : 'large'}
+        icon={<RightOutlined />}
+        css={css`
+          position: absolute;
+          right: ${xs ? '-10px' : '-20px'};
+          top: 45%;
+          z-index: 1000;
+        `}
+        onClick={() => scroll("right")}
+      />
+
       <Flex
+        ref={scrollRef}
         align="flex-start"
         css={css`
           overflow-x: auto;
+          scrollbar-width: none;
+          padding: 0px 12px;
       `}>
         {actors && actors.length > 0 ? actors.map(actor => (
           <PersonCard person={actor} css={css`
             flex-shrink: 0;
-            margin-right: 24px;
+            margin-right: 8px;
           `} />
         )) : <p>Нет актеров</p>}
       </Flex>
-    </>
+    </div>
   )
 }
